a3740edf3ba2264b5a4334396ce1769d6f95f261
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-17 10:05:15 -0800

    Support resource updating in chef 8.x and 9.x for mysql_database

--- cookbooks/mysql/providers/database.rb
+++ cookbooks/mysql/providers/database.rb
@@ -3,20 +3,23 @@ include Opscode::Mysql::Database
 action :flush_tables_with_read_lock do
   Chef::Log.info "mysql_database: flushing tables with read lock"
   db.query "flush tables with read lock"
-  #new_resource.updated_by_last_action(true)
+  new_resource.updated_by_last_action(true) if new_resource.respond_to? 'updated_by_last_action'
+  new_resource.updated = true if new_resource.updated
 end
 
 action :unflush_tables do
   Chef::Log.info "mysql_database: unlocking tables"
   db.query "unlock tables"
-  #new_resource.updated_by_last_action(true)
+  new_resource.updated_by_last_action(true) if new_resource.respond_to? 'updated_by_last_action'
+  new_resource.updated = true if new_resource.updated
 end
 
 action :create_db do
   unless @mysqldb.exists
     Chef::Log.info "mysql_database: Creating database #{new_resource.database}"
     db.query("create database #{new_resource.database}")
-    #new_resource.updated_by_last_action(true)
+    new_resource.updated_by_last_action(true) if new_resource.respond_to? 'updated_by_last_action'
+    new_resource.updated = true if new_resource.updated
   end
 end
 
