a0eb05074ad0cd4f90ed4c680beba0e45a8640eb
Ryan Geyer <rgeyer@its.jnj.com>
2011-01-21 11:36:30 -0800

    Better understanding of gem installation during compile time

--- cookbooks/mysql/libraries/database.rb
+++ cookbooks/mysql/libraries/database.rb
@@ -1,16 +1,13 @@
-def load_mysql_gem()
-  begin
-    require 'mysql'
-  rescue LoadError
-    Chef::Log.warn("Missing gem 'mysql'")
-  end
+begin
+  require 'mysql'
+rescue LoadError
+  Chef::Log.warn("Missing gem 'mysql'")
 end
 
 module Opscode
   module Mysql
     module Database
       def db
-        load_mysql_gem()
         @@db ||= ::Mysql.new new_resource.host, new_resource.username, new_resource.password
       end
     end
