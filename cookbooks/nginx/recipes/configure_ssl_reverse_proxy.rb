2ba78ca0e8fc8bfc252f30d3e666ac3834627759
Ryan Geyer <me@ryangeyer.com>
2011-03-01 10:26:01 -0800

    Needed to include the nginx::default before disabling the default site

--- cookbooks/nginx/recipes/configure_ssl_reverse_proxy.rb
+++ cookbooks/nginx/recipes/configure_ssl_reverse_proxy.rb
@@ -4,6 +4,8 @@ underscored_accept_fqdn=accept_fqdn.gsub(".","_")
 rsa_cert="#{node[:nginx][:dir]}/ssl/#{node[:nginx][:accept_fqdn]}.crt"
 rsa_key="#{node[:nginx][:dir]}/ssl/#{node[:nginx][:accept_fqdn]}.key"
 
+include_recpie "nginx::default"
+
 directory "#{node[:nginx][:dir]}/ssl" do
   mode 0644
   owner "root"
