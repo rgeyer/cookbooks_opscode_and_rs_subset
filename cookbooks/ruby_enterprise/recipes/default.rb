17576859bf7ffdd60c7c8ebd7d501a5433876e65
Ryan Geyer <me@ryangeyer.com>
2011-03-29 16:28:56 -0700

    wget > remote_file unless Chef.version >= 0.9.x

--- cookbooks/ruby_enterprise/recipes/default.rb
+++ cookbooks/ruby_enterprise/recipes/default.rb
@@ -29,8 +29,14 @@ include_recipe "build-essential"
   package pkg
 end
 
-remote_file "/tmp/ruby-enterprise-#{node[:ruby_enterprise][:version]}.tar.gz" do
-  source "#{node[:ruby_enterprise][:url]}.tar.gz"
+#remote_file "/tmp/ruby-enterprise-#{node[:ruby_enterprise][:version]}.tar.gz" do
+#  source "#{node[:ruby_enterprise][:url]}.tar.gz"
+#  not_if { ::File.exists?("/tmp/ruby-enterprise-#{node[:ruby_enterprise][:version]}.tar.gz") }
+#end
+
+bash "Download ruby enterprise installer" do
+  cwd "/tmp"
+  code "wget -q -O /tmp/ruby-enterprise-#{node[:ruby_enterprise][:version]}.tar.gz #{node[:ruby_enterprise][:url]}.tar.gz"
   not_if { ::File.exists?("/tmp/ruby-enterprise-#{node[:ruby_enterprise][:version]}.tar.gz") }
 end
 
